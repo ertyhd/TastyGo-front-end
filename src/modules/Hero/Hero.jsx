585eae720f5edb3cbe92ddf3ffc42b42717a8038
ertyhd <orionship@protonmail.com>
2023-07-08 23:22:52 +0300

    animation done

--- src/modules/Hero/Hero.jsx
+++ src/modules/Hero/Hero.jsx
@@ -1,5 +1,5 @@
-// import image from "../../assete/png/young-woman-eating-pasta-in-a-cafe1.png";
-// import { SvgSelector } from "../../shared/components/SvgSelector/SvgSelector";
+import image from "../../assete/png/young-woman-eating-pasta-in-a-cafe1.png";
+import { SvgSelector } from "../../shared/components/SvgSelector/SvgSelector";
 import styles from "./hero.module.scss";
 
 const Hero = () => {
@@ -13,14 +13,14 @@ const Hero = () => {
           <svg width="100vw" height="115px" />
         </div>
         <div className={styles.hero_container__backgroundRight}>
-          <svg width="100vw" height="115px" />
+          <svg width="110vw" height="115px" />
         </div>
         <div className={styles.hero_container__backgroundBlockLeft}>
           <svg width="100vw" height="115px" />
         </div>
       </div>
 
-      {/* <img
+      <img
         className={styles.hero_container__img}
         alt="young woman eating pasta in a cafe "
         src={image}
@@ -28,7 +28,7 @@ const Hero = () => {
       <button className={styles.hero_container__btn}>
         <span className={styles.hero_container__btn_text}>VIEW MENU</span>
         <SvgSelector id="arrowTopRight" />
-      </button> */}
+      </button>
     </div>
   );
 };

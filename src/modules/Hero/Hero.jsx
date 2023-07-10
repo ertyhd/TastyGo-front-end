50b30d36f616f6b4b472518a540b5443987da11e
ertyhd <orionship@protonmail.com>
2023-07-10 21:12:17 +0300

    change hero

--- src/modules/Hero/Hero.jsx
+++ src/modules/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import image from "../../assete/png/young-woman-eating-pasta-in-a-cafe1.png";
+import image from "../../assete/jpg/young-woman-eating-pasta-in-a-cafe 1.jpg";
 import { SvgSelector } from "../../shared/components/SvgSelector/SvgSelector";
 import styles from "./hero.module.scss";
 
@@ -6,6 +6,9 @@ const Hero = () => {
   return (
     <div className={styles.hero_container}>
       <div className={styles.hero_container__backgroundBlock}>
+        <div className={styles.hero_container__backgroundBlockLeft}>
+          <svg width="100vw" height="115px" />
+        </div>
         <div className={styles.hero_container__backgroundRight}>
           <svg width="100vw" height="115px" />
         </div>
@@ -13,31 +16,23 @@ const Hero = () => {
           <svg width="100vw" height="115px" />
         </div>
         <div className={styles.hero_container__backgroundRight}>
-          <svg width="110vw" height="115px" />
+          <svg width="100vw" height="115px" />
         </div>
         <div className={styles.hero_container__backgroundBlockLeft}>
           <svg width="100vw" height="115px" />
         </div>
       </div>
-
-      <img
-        className={styles.hero_container__img}
-        alt="young woman eating pasta in a cafe "
-        src={image}
-      />
-      <button className={styles.hero_container__btn}>
-        <span className={styles.hero_container__btn_text}>VIEW MENU</span>
-        <SvgSelector id="arrowTopRight" />
-      </button>
-      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-      <lottie-player
-        src="https://lottie.host/4e9ea6df-7a7d-4a69-9870-447a11d50626/RyxuAyIadb.json"
-        background="transparent"
-        speed=".5"
-        style={{ width: "300px", height: "300px" }}
-        loop
-        autoplay
-      ></lottie-player>
+      <div className={styles.hero_container__content}>
+        <img
+          className={styles.hero_container__img}
+          alt="young woman eating pasta in a cafe "
+          src={image}
+        />
+        <button className={styles.hero_container__btn}>
+          <span className={styles.hero_container__btn_text}>Make an order</span>
+          <SvgSelector id="arrowTopRight" />
+        </button>
+      </div>
     </div>
   );
 };

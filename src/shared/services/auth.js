33078aa3f87f1e1da29d25a3fc116ff78b372043
Sergey Toronchenko <105011827+ertyhd@users.noreply.github.com>
2023-11-18 12:09:18 +0300

    switch remote backend

--- src/shared/services/auth.js
+++ src/shared/services/auth.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3005/",
-  //  baseURL: "https://tastygo-back-end.onrender.com/",
+  // baseURL: "http://localhost:3005/",
+  baseURL: "https://tastygo-back-end.onrender.com/",
 });
 
 const setToken = (token) => {
